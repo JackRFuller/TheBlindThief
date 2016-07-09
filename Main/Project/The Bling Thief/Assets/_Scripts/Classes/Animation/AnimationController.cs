4ea979c86a074c5a90c257a1821d2dee704e2221
JackRFuller <jackrfuller@outlook.com>
2016-07-09 23:30:55 +0100

    Working PC Movement
    - PC moves horizontally & vertically
    - Basic Animation Controller

---
+++ Main/Project/The Bling Thief/Assets/_Scripts/Classes/Animation/AnimationController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour
+{
+
+	[SerializeField] protected Animator animController;
+
+    public virtual void TurnOnAnimation(string _targetAnim)
+    {
+
+        for (int i = 0; i < animController.parameters.Length; i++)
+        {
+            if (_targetAnim == animController.parameters[i].name)
+            {
+                animController.SetBool(_targetAnim, true);
+            }
+            else
+            {
+                string _paraName = animController.parameters[i].name;
+                animController.SetBool(_paraName, false);
+            }
+        }
+    }
+}
---
+++ Main/Project/The Bling Thief/Assets/_Scripts/Controllers/InputController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+using System.Collections;
+
+public class InputController : Singleton<InputController>
+{
+    private Vector3 targetPosition;
+    public Vector3 TargetPosition
+    {
+        get { return targetPosition; }
+    }
+
+    public static event Action PlayerInput; //Registers when theres been a valid input
+
+    void Update()
+    {
+        GetPlayerInput();
+    }
+
+    void GetPlayerInput()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            SendOutRaycastFromMousePosition();
+        }
+    }
+
+    void SendOutRaycastFromMousePosition()
+    {
+        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit _hit;
+
+        if (Physics.Raycast(_ray, out _hit, Mathf.Infinity))
+        {
+            if (_hit.collider.tag == "Node")
+            {
+                targetPosition = _hit.point;
+
+                if (PlayerInput != null)
+                    PlayerInput();
+            }
+        }
+    }
+}
