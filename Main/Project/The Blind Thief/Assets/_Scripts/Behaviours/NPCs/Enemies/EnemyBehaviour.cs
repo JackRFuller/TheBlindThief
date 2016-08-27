3dcf55c76d638b4e0d84d22e390b1a6a5c430f22
JackRFuller <jackrfuller@outlook.com>
2016-08-27 14:22:19 +0100

    Enemy Detection Wave
    Implemented Enemy Detection Wave

--- Main/Project/The Blind Thief/Assets/_Scripts/Behaviours/NPCs/Enemies/EnemyBehaviour.cs
+++ Main/Project/The Blind Thief/Assets/_Scripts/Behaviours/NPCs/Enemies/EnemyBehaviour.cs
@@ -15,6 +15,15 @@ public class EnemyBehaviour : MonoBehaviour
         aggro,
     }
 
+    [Header("Detection Waves")]
+    [SerializeField] private GameObject enemyDetectionWaves;
+    [SerializeField] private int numberOfDetectionWavesToSpawn;
+    private List<GameObject> detectionWaves;
+    [SerializeField] private float detectionWaveMaxSize;
+    [SerializeField] private float detectionWaveGrowthSpeed;
+    [SerializeField] private float detectionWaveFrequency; //How many seconds between each wave
+    private float detectionWaveTimer;    
+
     [Header("Sound Waves")]
     [SerializeField] private GameObject enemySoundWave;
     [SerializeField] private int numberToSpawn;
@@ -22,8 +31,56 @@ public class EnemyBehaviour : MonoBehaviour
 
     void Start()
     {
+        SpawnInDetectionWaves();
         SpawnInSoundWaves();
-    }   
+    }  
+    
+    void SpawnInDetectionWaves()
+    {
+        detectionWaves = new List<GameObject>();
+
+        for(int i = 0; i < numberOfDetectionWavesToSpawn; i++)
+        {
+            GameObject detectionWave = (GameObject)Instantiate(enemyDetectionWaves);
+
+            //Set Detection Wave Values
+            EnemyDetectionWaveBehaviour edwbScript = detectionWave.GetComponent<EnemyDetectionWaveBehaviour>();
+            edwbScript.MaxWaveSize = detectionWaveMaxSize;
+            edwbScript.WaveGrowthSpeed = detectionWaveGrowthSpeed;
+            edwbScript.OriginalParent = this.transform;
+
+            detectionWaves.Add(detectionWave);
+           
+            detectionWave.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        DetectionWaveTimer();
+    }
+
+    void DetectionWaveTimer()
+    {
+        detectionWaveTimer += Time.smoothDeltaTime;
+        if(detectionWaveTimer >= detectionWaveFrequency)
+        {
+            SendOutDetectionWave();
+            detectionWaveTimer = 0;
+        }
+    }
+
+    void SendOutDetectionWave()
+    {
+        for(int i = 0; i < detectionWaves.Count; i++)
+        {
+            if(!detectionWaves[i].activeInHierarchy)
+            {
+                detectionWaves[i].SetActive(true);
+                break;
+            }
+        }
+    }
 
     void SpawnInSoundWaves()
     {
---
+++ Main/Project/The Blind Thief/Assets/_Scripts/Behaviours/NPCs/Enemies/EnemyDetectionWaveBehaviour.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDetectionWaveBehaviour : MonoBehaviour
+{
+    private Vector3 originalSize;
+    private Transform originalParent;
+    public Transform OriginalParent { get { return originalParent; } set { originalParent = value; } }
+
+    private float waveGrowthSpeed; //Defines how quickly the wave grows
+    public float WaveGrowthSpeed { get { return waveGrowthSpeed; } set { waveGrowthSpeed = value; } }
+    private float maxWaveSize; // Defines the maximum size a detection wave can get
+    public float MaxWaveSize { get { return maxWaveSize; } set { maxWaveSize = value; } }
+
+    private Vector3 maxSize;
+
+    private bool isGrowing;
+
+    void Start()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        originalSize = transform.localScale;
+    }
+
+	void OnEnable()
+    {
+        InitiateGrowth();
+    }
+
+    void InitiateGrowth()
+    {
+        transform.parent = null;
+        maxSize = new Vector3(maxWaveSize, maxWaveSize, maxWaveSize);
+        isGrowing = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (isGrowing)
+            GrowWave();
+    }   
+
+    void GrowWave()
+    {
+        Vector3 currentSize = transform.localScale;
+        float speed = waveGrowthSpeed * Time.deltaTime;
+        Vector3 newSize = new Vector3(currentSize.x + speed, currentSize.y + speed, currentSize.z + speed);
+        transform.localScale = newSize;
+
+        if(newSize.x >= maxWaveSize)
+        {
+            ResetWave();
+        }
+    }
+
+    void ResetWave()
+    {
+        isGrowing = false;
+
+        transform.localScale = originalSize;
+        transform.parent = originalParent;
+        transform.localPosition = Vector3.zero;
+
+        gameObject.SetActive(false);
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Player"))
+        {
+            Debug.Log("Detected Player");
+            originalParent.SendMessage("HitBySoundWave", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
